0c47380c989b172b6fd3ba3390dfa7e2468d633c
wsloong <wslong2020@hotmail.com>
2020-06-09 20:02:08 +0800

    add: 20200609

--- double_pointer/20200609/README.md
+++ double_pointer/20200609/README.md
@@ -3,5 +3,5 @@
 ### [26.删除排序数组中的重复项](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/)
 ![remove-duplicates-from-sorted-array](./today/images/remove-duplicates-from-sorted-array.png)
 
-### [904.水果成篮](https://leetcode-cn.com/problems/fruit-into-baskets/)
+### [904.水果成篮](https://leetcode-cn.com/problems/fruit-into-baskets/) `该题第一次接触，虽然理解了，但是要多复习`
 ![fruit-into-baskets](./today/images/fruit-into-baskets.png)
--- double_pointer/20200609/today/fruit-into-baskets.go
+++ double_pointer/20200609/today/fruit-into-baskets.go
@@ -1,4 +1,6 @@
 /*
+904. 水果成篮
+
 在一排树中，第 i 棵树产生 tree[i] 型的水果。
 你可以从你选择的任何树开始，然后重复执行以下步骤：
 	把这棵树上的水果放进你的篮子里。如果你做不到，就停下来。
@@ -41,11 +43,79 @@
 */
 
 func totalFruit(tree []int) int {
-	length := len(tree)
-	if length <= 2 {
-		return length
+	result, i := 0, 0
+
+	count := newCounter()
+
+	for j := range tree {
+		count.Add(tree[j], 1)
+		for count.Size() >= 3 {
+			count.Add(tree[i], -1)
+			if count.Get(tree[i]) == 0 {
+				count.Remove(tree[i])
+			}
+			i++
+		}
+		result = max(result, j - i + 1)
 	}
 
-	
+	return result
+}
 
+func max(v1, v2 int) int {
+	if v1 >= v2 {
+		return v1
+	}
+
+	return v2
+}
+
+type Counter struct {
+	item map[int]int
+}
+
+func newCounter() *Counter {
+	return &Counter{
+		item: make(map[int]int),
+	}
+}
+
+func (c *Counter) Get(k int) int {
+	value, ok := c.item[k]
+	if ok {
+		return value
+	}
+	return 0
+}
+
+func (c *Counter) Add(k, v int) {
+	c.item[k] = c.Get(k) + v
+}
+
+func (c *Counter) Size() int {
+	return len(c.item)
+}
+
+func (c *Counter) Remove(k int) {
+	delete(c.item, k)
+}
+
+
+// ====== 早上的  删除排序数组中的重复项 ===
+/*
+思路回顾
+	题目虽然是删除但是跟确切来说是交换
+	使用双指针，i, j 
+	如果nums[i] != nums[j], i++, nums[i] = nums[j]
+*/
+
+func removeDuplicates(nums []int) int {
+	length, i := len(nums), 0
+	for j := 1; j < length; j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i]= nums[j]
+		}
+	}
+	return i + 1
 }
