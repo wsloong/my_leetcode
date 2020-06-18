af0a804e350ba90d50aa0c390313ebe47ba68eff
wsloong <wslong2020@hotmail.com>
2020-06-18 18:05:25 +0800

    add: 20200618

--- coding_interviews/04二维数组中的查找/er-wei-shu-zu-zhong-de-cha-zhao-lcof.go
+++ coding_interviews/04二维数组中的查找/er-wei-shu-zu-zhong-de-cha-zhao-lcof.go
@@ -19,3 +19,23 @@ func findNumberIn2DArray(matrix [][]int, target int) bool {
 	}
 	return false
 }
+
+// 从左下角开始判断
+func findNumberIn2DArrayFromLeftBottom(matrix [][]int, target int) bool {
+	// 特殊判断
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+
+	row, col := len(matrix)-1, 0
+	for row >= 0 && col < len(matrix[0]) {
+		if matrix[row][col] < target {
+			col++
+		} else if matrix[row][col] > target {
+			row--
+		} else {
+			return true
+		}
+	}
+	return false
+}
